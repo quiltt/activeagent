c8e634c52accd73d35283c02abf4b28bd360b606
Justin Bowen <JusBowen@gmail.com>
2025-06-11 23:18:23 -0700

    Refactoring tests

---
+++ test/action_prompt/message_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+module ActiveAgent
+  module ActionPrompt
+    class MessageTest < ActiveSupport::TestCase
+      test "array for message hashes to messages" do
+        messages = [
+          {content: "Instructions", role: :system},
+          {content: "This is a message", role: :user}
+        ]
+        assert Message.from_messages(messages).first.is_a? Message
+      end
+    end
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,11 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
+
 require "jbuilder"
 require_relative "../test/dummy/config/environment"
-ActiveRecord::Migrator.migrations_paths = [ File.expand_path("../test/dummy/db/migrate", __dir__) ]
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
 require "rails/test_help"
 require "vcr"
-require "active_agent/action_prompt/prompt"
 
 VCR.configure do |config|
   config.cassette_library_dir = "fixtures/vcr_cassettes"
@@ -16,7 +16,7 @@ end
 
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
-  ActiveSupport::TestCase.fixture_paths = [ File.expand_path("fixtures", __dir__) ]
+  ActiveSupport::TestCase.fixture_paths = [File.expand_path("fixtures", __dir__)]
   ActionDispatch::IntegrationTest.fixture_paths = ActiveSupport::TestCase.fixture_paths
   ActiveSupport::TestCase.file_fixture_path = File.expand_path("fixtures", __dir__) + "/files"
   ActiveSupport::TestCase.fixtures :all
