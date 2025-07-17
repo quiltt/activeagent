5bec197640444086dab5dbd8d6de4eb11288155e
Justin Bowen <JusBowen@gmail.com>
2025-07-17 08:41:34 -0700

    Slight modification to the test to demo expected/common use case

--- test/dummy/app/agents/scoped_agents/translation_agent_with_custom_instructions_template.rb
+++ test/dummy/app/agents/scoped_agents/translation_agent_with_custom_instructions_template.rb
@@ -1,16 +1,23 @@
 module ScopedAgents
   class TranslationAgentWithCustomInstructionsTemplate < ApplicationAgent
     generate_with :openai, instructions: {
-      template: :custom_instructions, locals: { from: "English", to: "French" }
+      template: :custom_instructions, locals: {from: "English", to: "French"}
     }
 
+    before_action :add_custom_instructions
+
     def translate
-      @additional_instruction = "translation additional instruction"
       prompt
     end
 
     def translate_with_overridden_instructions
-      prompt(instructions: { template: :overridden_instructions })
+      prompt(instructions: {template: :overridden_instructions})
+    end
+
+    private
+
+    def add_custom_instructions
+      @additional_instruction = "translation additional instruction"
     end
   end
 end
